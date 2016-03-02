241c8e00600a6eca3928c194bb67632572c1d1e0
Emily MacGowan <emilymac@Emilys-MacBook-Pro.local>
2016-03-01 22:00:51 -0500

    first commit

---
+++ phpwebsite.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>PHP_Learning</title>
+	</head>
+	<body>
+		<p>
+			<?php
+				echo "My first line of PHP!";
+				$myName = "Emily";
+				$myAge = 21;
+				// this is a comment for PHP code
+			?>
+		</p>
+	</body>
+</html>
+
+<!-- PHP runs on the same same computer as the website you're visiting (the server) used to handle cookies, run tasks or perform calculations with data from the website-->
+<!-- The echo() function outputs one or more strings. Make sure the end of the file has .php -->
+<!-- echo "Hello," . " " . "world"; - the dot syntax for PHP is how you concatenate information
+it is the equivalent of + in javascript
+
+Variables
+
+Variable naes in PHP start with a dollar sign $
+$myName = "Beyonce";
+$myAge = 32;
+
+Semicolons
+
+Semicolons are used at the end of each statement
+
+Conditionals and control flow
+Control flow is how a program decides to perform one action instead of another.
+if and else can do this
