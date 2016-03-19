67425f8007e40eaef021d6cfff943dd864e54a0a
Emily MacGowan <emilymac@Emilys-MacBook-Pro.local>
2016-03-18 20:52:16 -0400

    new work

---
+++ index1.php
@@ -0,0 +1,44 @@
+
+<head>
+	<title>ENTechPortal</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,900|Patrick+Hand' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+	<nav>
+		<ul class= "middlenav">
+			<li>Home</li>
+			<li>About</li>
+			<li>Contact</li>
+			<a href="#">Login</a>
+		</ul>
+
+	</nav>
+	</header>
+	<section class = "splash">
+		<div class = "logo">
+		<!-- below is the entech logo -->
+			<img src="images2/enTechLogo4.png"> 
+		</div>
+		<div id="container">
+		<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+
+				<legend>Login</legend>
+				<br>
+				<input type='hidden' name='submitted' id='submitted' value='1'/>
+	 
+				<label for='username'>Username</label>
+				<input type='text' name='username' id='username'  maxlength="50" />
+	 			<br>
+				<label for='password'>Password</label>
+				<input type='password' name='password' id='password' maxlength="50" />
+	 			<br>
+	 			<br>
+				<input type='submit' name='login' value='login' />
+		</form>
+		</div>
+		<!-- this is where I will place the content for the first page -->
+		<!-- <h1>Where seniors come to learn about technology</h1> -->
+	</section>
+</body>
