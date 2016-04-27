fbf9737e0d2dd70bf147cab5011ca69d59d60ba3
emacgowa <emily_macgowan@hotmail.com>
2016-04-27 11:12:15 -0400

    php registration is now working

--- registerusers.php
+++ registerusers.php
@@ -1,37 +1,32 @@
- <?php
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-
-
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$databasename = "users";
-$connection= mysqli_connect($servername, $username, $password, $databasename) or die ("could not connect to mysql");
-
-if(isset($_POST['signup'])) { // 
-	//include_once("users.php");
-
-	$username = strip_tags($_POST['username']);
+if(isset($_POST['signup'])) {
+	$myusername = strip_tags($_POST['username']);
 	$mypassword = strip_tags($_POST['password']);
 	$email = strip_tags($_POST['email']);
 	$active = 1;
 
-	$username = stripslashes($username);
-	$password = stripslashes($password);
+	$myusername = stripslashes($myusername);
+	$mypassword = stripslashes($mypassword);
 	$email = stripslashes($email);
 
+	include("mysqldatabase.php");
+	$connection= mysqli_connect($servername, $dbusername, $dbpassword, $databasename) or die ("could not connect to mysql");
+
 	// stop people from injecting sql commands into string
-	$username = mysqli_real_escape_string($connection, $username);
-	$password = mysqli_real_escape_string($connection, $password);
+	$myusername = mysqli_real_escape_string($connection, $myusername);
+	$mypassword = mysqli_real_escape_string($connection, $mypassword);
 	$email = mysqli_real_escape_string($connection, $email);
 
 	// if username is equal to nothing, please insert username
-	if($username == "") {
+	if($myusername == "") {
 		echo "Please insert a username";
 		return;
 	}
 
-	if($password == "") {
+	if($mypassword == "") {
 		echo "Please choose a password";
 		return;
 	}
@@ -42,12 +37,13 @@ if(isset($_POST['signup'])) { //
 		return;
 	}
 
-	$password = md5($password);
+	$mypassword = md5($mypassword);
 
-	$sql_store = "INSERT into users (username, password, email, active) VALUES ('$username', '$password', '$email', '$active')";
+
+	$sql_store = "INSERT into users (username, password, email, active) VALUES ('$myusername', '$mypassword', '$email', '$active')";
 
 	// check if people used the same username and email
-	$sql_fetch_username = "SELECT username FROM users WHERE username = '$username'"; // if this returns ttrue then that means that there is a username there and it will produce an error
+	$sql_fetch_username = "SELECT username FROM users WHERE username = '$myusername'"; // if this returns ttrue then that means that there is a username there and it will produce an error
 
 	// check if the email already exists in the database
 	$sql_fetch_email = "SELECT email FROM users WHERE email = '$email'";
@@ -67,87 +63,8 @@ if(isset($_POST['signup'])) { //
 	}
 
 	mysqli_query($connection, $sql_store);
+	mysqli_close($connection);
 	header("Location: index1.php");
+	exit;
 }
-
-
-// echo "working";
-// 	$servername = "localhost";
-// 	$username = "root";
-// 	$password = "root";
-// 	$databasename = "users";
-// $connection= mysqli_connect($servername, $username, $password, $databasename) or die ("could not connect to mysql");
-// echo "yep";
-// if (!$connection) {
-// 	echo "no connection";
-// } else {
-// 	echo "yes connection";
-
-// }
-
-// $sql1 = "SELECT * FROM users WHERE username = $username";
-// echo "fun";
-// $result = mysqli_query($connection,$sql1);
-// echo "this is fun";
-// echo $result->num_rows;
-
-// 	$myusername = $_POST['username'];
-// 	$mypassword = $_POST['password'];
-// 	$email = $_POST['email'];
-// 	$active = 1;
-
-// 	$sql2 = "INSERT INTO users (username, password, email, active) VALUES ('$myusername', '$mypassword', '$email', '$active')";
-
-// 	$result = $connection -> query($sql1);
-
-/* *************************************************
-/*
-//if(isset($_POST['signup'])){
-	$servername = "localhost";
-	$username = "root";
-	$password = "root";
-	$databasename = "users";
-
-	// mysqli is an object in php that connects php to mysql
-	// the order of the things in the brackets matter! think of it as a "trickle down approach", you give the servername, you login with the username and password and then you give the database name you want to work with
-	$connection= mysqli_connect($servername, $username, $password, $databasename) or die ("could not connect to mysql");
-
-	echo "hello";
-
-	if ($connection->connect_error) {
-	    die("Connection failed: " . $connection->connect_error);
-	}
-
-	echo "Connected successfully";
-
-
-
-	$myusername = $_POST['username'];
-	$mypassword = $_POST['password'];
-	$email = $_POST['email'];
-	$active = 1;
-
-	// $password = md5($password);
-
-	// NEXT STEP IS TO CHECK WHETHER USERNAME AND EMAIL have already been taken
-	$sql1 = "SELECT * FROM users WHERE username='$myusername'";
-	$sql2 = "INSERT INTO users (username, password, email, active) VALUES ('$myusername', '$mypassword', '$email', '$active')";
-	
-	$result = $connection -> query($sql1);
-
-	if(!$result) {
-		die($connection -> error)
-	}
-
-	if($result -> num_rows > 0) {
-		echo "duplicate user";
-	} else {
-		$connection -> query($sql2);
-	}
-	// users is the name of the table
-
-//}
-*/
-
-
 ?>
